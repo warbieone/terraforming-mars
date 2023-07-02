128bddb89033e3b73c571099c37a1dffc9afec06
warbieone <owen.warburton@gmail.com>
2023-07-02 19:43:43 +1200

    remove corps

--- src/server/Game.ts
+++ src/server/Game.ts
@@ -313,9 +313,9 @@ export class Game implements IGame, Logger {
         gameOptions.initialDraftVariant ||
         gameOptions.ceoExtension) {
 
-          const specificCardsOwen = ['Teractor','Celestic','Stormcraft Incorporated','Mining Guild','Recyclon','Terralabs Research','Septem Tribus','Valley Trust','Cheung Shing MARS','Thorgate'];
-          const specificCardsLaura = ['Saturn Systems','Inventrix','PhoboLog','Viron','Morning Star Inc.','Factorum','Aridor','Aphrodite','Point Luna','Splice'];
-          const specificCardsJoel = ['Mons Insurance','Astrodrill','Lakefront Resorts','Pristar','CrediCor','Poseidon','Manutech','Pharmacy Union','Philares','EcoLine'];
+          const specificCardsOwen = ['Teractor','Celestic','Mining Guild','Recyclon','Terralabs Research','Septem Tribus','Valley Trust','Cheung Shing MARS','Thorgate'];
+          const specificCardsLaura = ['Saturn Systems','Inventrix','PhoboLog','Morning Star Inc.','Factorum','Aridor','Aphrodite','Point Luna','Splice'];
+          const specificCardsJoel = ['Mons Insurance','Astrodrill','Pristar','CrediCor','Poseidon','Manutech','Pharmacy Union','Philares','EcoLine'];
          
           if (player.name !== 'Owen T' && player.name !== 'Laura T' && player.name !== 'Joel T') {
               for (let i = 0; i < gameOptions.startingCorporations; i++) {
